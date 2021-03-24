e45ea569e24757eaf9c61ae3944d45020168cde0
Ynda Jas <yndajas@gmail.com>
2021-03-24 02:51:30 +0000

    commit tree a8ce88c6826c59d4dfa8e86cd0b5e4b685efd66e

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Your code here
+
+function mapToNegativize(sourceArray) {
+    const mutatedArray = [];
+    for (const element of sourceArray) {
+        mutatedArray.push(element * -1);
+    }
+    return mutatedArray;
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray;
+}
+
+function mapToDouble(sourceArray) {
+    const mutatedArray = [];
+    for (const element of sourceArray) {
+        mutatedArray.push(element * 2);
+    }
+    return mutatedArray;
+}
+
+function mapToSquare(sourceArray) {
+    const mutatedArray = [];
+    for (const element of sourceArray) {
+        mutatedArray.push(element * element);
+    }
+    return mutatedArray;
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+    for (const element of sourceArray) {
+        startingPoint += element;
+    }
+    return startingPoint;
+}
+
+function reduceToAllTrue(sourceArray) {
+    let allTrue = true;
+    for (const element of sourceArray) {
+        if (!!element === false) {
+            allTrue = false;
+        }
+    }
+    return allTrue;
+}
+
+function reduceToAnyTrue(sourceArray) {
+    let anyTrue = false;
+    for (const element of sourceArray) {
+        if (!!element === true) {
+            anyTrue = true;
+        }
+    }
+    return anyTrue;
+}
